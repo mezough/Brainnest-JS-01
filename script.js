01e1a7f6a336141783ff602f743f78f4bfc2449a
mezough <118981643+mezough@users.noreply.github.com>
2023-05-02 20:50:03 +0200

    final

--- script.js
+++ script.js
@@ -1,47 +1,71 @@
-
-// begin with a function called 'computerPlay' that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-
 let words = ["rock", "paper", "scissors"];
 
+
 function computerPlay() {
-    for (let i = words.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = words[i];
-        words[i] = words[j];
-        words[j] = temp;
-    }
-    return words[0];
+    return words[(Math.floor(Math.random() * 3))]
 }
 
-// User Input:
+let playerScore = 0;
+let computerScore = 0;
 
-let userInput = prompt("Choose: rock, paper or scissors?").toLowerCase();
-
-
-// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection:
-let computerSelection = computerPlay();
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "Lets do it again :)";
+        return "It's a Draw!";
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-        return "Player wins!";
+        playerScore++;
+        return "You win!";
     } else {
+        computerScore++;
         return "Computer wins!";
     }
 }
 
-console.log("Player: " + userInput);
-console.log("Computer: " + computerSelection);
-console.log(playRound(userInput, computerSelection));
+function game() {
+alert ('Hello My Friend \n Welcome to my GAME ; ) \n \n We will Play 5 rounds "Paper-Rock-Scissor" and you can leave any time \n Click -OK- if you are ready');
+    let round = 1;
+    while (round <= 5) {
+        let playerSelection = prompt("Round " + round + ": Choose rock, paper or scissors?");
+        if (playerSelection === null) {
+            let answer = prompt("Do you want to cancel the game? (Y/N)");
+            if (answer === "y".toLowerCase().trim()) {
+                alert('See You Later : ) \n Bye');
+                return;
+            } else if (answer === "n".toLowerCase().trim()) {
+                alert('GOOD : ) \n Lets PLAY');
+                continue;
+            } else {
+                alert("Invalid input. Please enter Y or N.");
+                continue;
+            }
+        } else if (playerSelection === '') {
+            alert('Empty Field \n Try Again');
+            continue;
+        }
 
-// Write a NEW function called game():
+        playerSelection = playerSelection.toLowerCase().trim();
+        if (!words.includes(playerSelection)) {
+            alert('Wrong Input \n Try Again');
+            continue;
+        }
+        let computerSelection = computerPlay();
+        console.log("Round " + round + ": Player: " + playerSelection + " vs Computer: " + computerSelection);
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
+        if (result !== "It's a Draw!") {
+            round++;
+        }
+    }
+    if (playerScore > computerScore) {
+        console.log("You win the game! Player score: " + playerScore + ", Computer score: " + computerScore);
+    } else if (playerScore < computerScore) {
+        console.log("Computer wins the game! Player score: " + playerScore + ", Computer score: " + computerScore);
+    } else {
+        console.log("It's a tie! Player score: " + playerScore + ", Computer score: " + computerScore);
+    }
+}
 
-// function game() {
-//     for (let i = 1; i <= 5; i++) {
-      
-// }
-// }
+game();
