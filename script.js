54dcee6d6ea5a57d77c77927b274cc02bf6a82de
mezough <118981643+mezough@users.noreply.github.com>
2023-05-04 22:18:35 +0200

    userInput() inserted

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let words = ["rock", "paper", "scissors"];
 
-
 function computerPlay() {
-    return words[(Math.floor(Math.random() * 3))]
+    return words[Math.floor(Math.random() * 3)];
 }
 
 let playerScore = 0;
 let computerScore = 0;
+let round = 1;
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -24,35 +24,45 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-alert ('Hello My Friend \n Welcome to my GAME ; ) \n \n We will Play 5 rounds "Paper-Rock-Scissor" and you can leave any time \n Click -OK- if you are ready');
-    let round = 1;
-    while (round <= 5) {
-        let playerSelection = prompt("Round " + round + ": Choose rock, paper or scissors?");
-        if (playerSelection === null) {
-            let answer = prompt("Do you want to cancel the game? (Y/N)");
-            if (answer === "y".toLowerCase().trim()) {
-                alert('See You Later : ) \n Bye');
-                return;
-            } else if (answer === "n".toLowerCase().trim()) {
-                alert('GOOD : ) \n Lets PLAY');
-                continue;
-            } else {
-                alert("Invalid input. Please enter Y or N.");
-                continue;
-            }
-        } else if (playerSelection === '') {
-            alert('Empty Field \n Try Again');
-            continue;
+function userInput() {
+    let playerSelection = prompt("Round " + round + ": Choose rock, paper or scissors?");
+    if (playerSelection === null) {
+        let answer = prompt("Do you want to cancel the game? (Y/N)");
+        if (answer.toLowerCase().trim() === "y") {
+            alert("See You Later : ) \n Bye");
+        } else if (answer.toLowerCase().trim() === "n") {
+            alert("Good Choise : ) \n Let's PLAY");
+            return userInput();
+        } else {
+            alert("Invalid input. Please enter Y or N.");
+            return userInput();
         }
-
+    } else if (playerSelection.trim() === "") {
+        alert("Empty Field \n Try Again");
+        return userInput();
+    } else {
         playerSelection = playerSelection.toLowerCase().trim();
         if (!words.includes(playerSelection)) {
-            alert('Wrong Input \n Try Again');
-            continue;
+            alert("Wrong Input \n Try Again");
+            return userInput();
+        }
+    }
+    return playerSelection;
+}
+
+function game() {
+    alert(
+        'Hello My Friend \n Welcome to my GAME ; ) \n \n We will Play 5 rounds "Paper-Rock-Scissor" and you can leave any time \n Click -OK- if you are ready'
+    );
+
+    while (round <= 5) {
+        let playerSelection = userInput();
+        if (playerSelection === null) {
+            return;
         }
         let computerSelection = computerPlay();
-        console.log("Round " + round + ": Player: " + playerSelection + " vs Computer: " + computerSelection);
+        console.log("Round " + round + ": Player: " + playerSelection + " vs Computer: " + computerSelection
+        );
         let result = playRound(playerSelection, computerSelection);
         console.log(result);
         if (result !== "It's a Draw!") {
@@ -60,11 +70,14 @@ alert ('Hello My Friend \n Welcome to my GAME ; ) \n \n We will Play 5 rounds "P
         }
     }
     if (playerScore > computerScore) {
-        console.log("You win the game! Player score: " + playerScore + ", Computer score: " + computerScore);
+        alert("You win the game! Player score: " + playerScore + ", Computer score: " + computerScore + "\n Press F5 to rematch "
+        );
     } else if (playerScore < computerScore) {
-        console.log("Computer wins the game! Player score: " + playerScore + ", Computer score: " + computerScore);
+        alert("Computer wins the game! Player score: " + playerScore + ", Computer score: " + computerScore+ "\n Press F5 to rematch "
+        );
     } else {
-        console.log("It's a tie! Player score: " + playerScore + ", Computer score: " + computerScore);
+        alert("It's a Draw! Player score: " + playerScore + ", Computer score: " + computerScore+ "\n Press F5 to rematch "
+        );
     }
 }
 
